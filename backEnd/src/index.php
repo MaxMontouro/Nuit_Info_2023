698da53b3bf00147fccc87969ddacace26458e91
Clément MOURGUE <cmourgue@iutbayonne.univ-pau.fr>
2023-12-07 19:07:03 +0100

    Ajout de fichiers de base dans le dossier src de la section backEnd

---
+++ backEnd/src/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head> 
+        <title>Tp7 php</title>
+    </head>
+<body>
+    <h1 style="text-align:center; font-style: italic;">TP7</h1>
+    <br/>
+    <form action="login.php" method="post">
+        Votre login : 
+        <input type="text" name="login">
+        <br/> <br/>
+        Votre mot de passe : 
+        <input type="password" name="pwd">
+        <br/> <br/>
+        <input type="submit" value="Connexion">
+    </form>
+</body>
+</html>
